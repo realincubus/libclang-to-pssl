a818f38e31f15d6bf42babe01a19523c15caec67
realincubus <kemnitz.stefan@googlemail.com>
2016-08-25 12:56:29 +0200

    work in progress

--- main.cpp
+++ main.cpp
@@ -1,41 +1,83 @@
 
+
 #include <clang-c/Index.h>
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cstring>
 
 using namespace std;
 
+#if 1
+typedef std::vector<std::string> plugin_list;
+
+inline void add_load_flags_for_lib( plugin_list& list, std::vector<const char*>& pointer_flags )
+{
+  for( auto& element : list ){
+    std::cout << " exec" << std::endl;
+    pointer_flags.push_back(strdup((std::string("-fplugin=") + element).c_str()));
+    //pointer_flags.push_back(strdup(("-Xclang -load -Xclang "s + element).c_str()));
+    //pointer_flags.push_back(element.c_str());
+  }
+  
+}
+#endif
+
 int main(int argc, char** argv) {
   CXIndex clang_index = clang_createIndex(0,0);
-  std::string filename = "test.cpp";
+  std::string filename = "main.cpp";
   std::vector<const char*> pointer_flags;
 
-  //pointer_flags.push_back("-Xclang");
-  //pointer_flags.push_back("-load");
-
-  pointer_flags.push_back("-Xclang");
-  pointer_flags.push_back("-add-plugin");
-  pointer_flags.push_back("-Xclang");
-  pointer_flags.push_back("clang-tidy");
-
-  pointer_flags.push_back("-Xclang");
-  pointer_flags.push_back("-plugin-arg-clang-tidy");
-  pointer_flags.push_back("-Xclang");
-  pointer_flags.push_back("-checks=*");
+  plugin_list lib_list;
+  lib_list.push_back( "/home/incubus/dev/ClanPlugin/lib/ClanPlugin.so" );
+  //lib_list.emplace_back( "/home/incubus/llvm_patch_test/build/lib/clangTidyPluginLM.so" ));
   
-  CXTranslationUnit tu = clang_createTranslationUnit(clang_index, filename.c_str() ) ;
+  //add_load_flags_for_lib( lib_list, pointer_flags );
+
+#if 0
+  for( auto& element : pointer_flags ){
+      std::cout << element << std::endl;
+  }
+#endif
+
+  pointer_flags.push_back("-std=c++11");
+
+#if 0
+  pointer_flags.push_back("-Xclang");
+  pointer_flags.push_back("-plugin-arg-clan");
+  pointer_flags.push_back("-Xclang");
+  pointer_flags.push_back("-redirect-stdout");
+  pointer_flags.push_back("-Xclang");
+  pointer_flags.push_back("-plugin-arg-clan");
+  pointer_flags.push_back("-Xclang");
+  pointer_flags.push_back("/dev/null");
+
+  pointer_flags.push_back("-Xclang");
+  pointer_flags.push_back("-plugin-arg-clan");
+  pointer_flags.push_back("-Xclang");
+  pointer_flags.push_back("-redirect-stderr");
+  pointer_flags.push_back("-Xclang");
+  pointer_flags.push_back("-plugin-arg-clan");
+  pointer_flags.push_back("-Xclang");
+  pointer_flags.push_back("/dev/null");
+#endif
+
+  std::cout << "starting to parse the tu" << std::endl;
+  
+  CXTranslationUnit tu = clang_createTranslationUnit(clang_index, strdup(filename.c_str()) ) ;
 
   auto clang_translation_unit_error = clang_parseTranslationUnit2(
       clang_index, 
-      filename.c_str(), 
-      &pointer_flags[0], 
-      pointer_flags.size(),
+      strdup(filename.c_str()), 
+      nullptr,//&pointer_flags[0], 
+      0,//pointer_flags.size(),
       nullptr, 
       0,
       clang_defaultEditingTranslationUnitOptions(),
       &tu
   );
+
+  std::cout << "done parsing" << std::endl;
 
   if ( clang_translation_unit_error != CXError_Success  ) {
     std::cout << "could not parse the tu ec: " << clang_translation_unit_error << std::endl;
@@ -44,12 +86,19 @@ int main(int argc, char** argv) {
 
   unsigned int n_diagnostics = clang_getNumDiagnostics( tu );
 
+  std::cout << "number of diagnostics " << n_diagnostics << std::endl;
+
+  ofstream out("diag_results.txt");
+  
   for (int i = 0; i < n_diagnostics; ++i){
     auto diag = clang_getDiagnostic( tu, i );
     auto diag_cxstring = clang_formatDiagnostic( diag, CXDiagnostic_DisplaySourceLocation );
     auto str = clang_getCString( diag_cxstring );
-    std::cout << str << std::endl;
+    out << str << std::endl;
   }
+
+  out.close();
+
 
   return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,20 +1,12 @@
 
+int main(){
 
-
-void foo()
-{
-   
   double x[100];
   double y[100];
 
   for (int i = 0; i < 100; ++i){
-    x[i] = 
-      y[i];
-  } 
-}
-
-int main(){
-
+    x[i] = y[i];
+  }
 
   return 0;
 }
